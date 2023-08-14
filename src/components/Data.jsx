9ccce05b3a13641380a2b0a83431c3cc37b9e682
yuanyi <wyuanyi1029@gmail.com>
2023-08-15 07:53:50 +0800

    new Boostrap table, pagination + sorting

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 function Data() {
   const [posts, setPosts] = useState([]);
@@ -12,53 +13,62 @@ function Data() {
       .then(data => setPosts(data.posts));
   }, []);
 
+  const categoriesFormatter = (categories) => {
+    const categoryNames = categories.map(category => category.name);
+    return <ul>{categoryNames.map(name => <li key={name}>{name}</li>)}</ul>;
+  };
+
+  const dateFormatter = (dateString) => {
+    const options = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      timeZone: 'UTC'
+    };
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-GB', options);
+  };
+
+  const authorFormatter = (author) => (
+    <div>
+      <img src={author.avatar} alt={author.name} style={{ width: '20px', height: '20px', marginRight: '5px' }} />
+      {author.name}
+    </div>
+  );
+
   const columns = [{
     dataField: 'title',
-    text: 'Title'
+    text: 'Title',
+    sort: true
   }, {
     dataField: 'publishDate',
-    text: 'Publish Date'
+    text: 'Publish Date',
+    formatter: dateFormatter,
+    sort: true
   }, {
     dataField: 'author',
-    text: 'Author'
+    text: 'Author',
+    formatter: authorFormatter,
+    sort: true
   }, {
     dataField: 'summary',
-    text: 'Summary'
+    text: 'Summary',
+    sort: true
   }, {
     dataField: 'categories',
-    text: 'Categories'
+    text: 'Categories',
+    formatter: categoriesFormatter,
+    sort: true
   }];
   
   return (
     <div style={{ padding: '20px '}}>
       <h2>List Of Posts</h2>
-      <Table>
-        <thead style={{ fontSize: '12px'}}>
-          <tr>
-            {/* <th>#</th> */}
-            <th>Title</th>
-            <th>Publish Date</th>
-            <th>Author</th>
-            <th>Summary</th>
-            <th>Categories</th>
-          </tr>
-        </thead>
-        <tbody style={{ fontSize: '10px'}}>
-          { 
-          posts.map(each => 
-            <tr>
-              <td>{ each.title }</td>
-              <td>{ each.publishDate }</td>
-              <td>{ each.author.name }</td>
-              <td>{ each.summary }</td>
-              <td>
-                { each.categories.map(eachcategory => <li>{eachcategory.name}</li>)}
-              </td>
-            </tr>
-          ) 
-          }
-        </tbody>
-      </Table>
+      <div style={{ fontSize: '10px'}}>
+        <BootstrapTable keyField="id" data={posts} columns={columns} striped hover condensed pagination={paginationFactory()}/>
+      </div>
     </div>
   );
 }
