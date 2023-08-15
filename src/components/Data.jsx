3ec9b6f8fecbcb4597949b149baeeb7be9e76748
yuanyi <wyuanyi1029@gmail.com>
2023-08-15 22:30:50 +0800

    filter feature

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import Table from 'react-bootstrap/Table';
+import Form from 'react-bootstrap/Form';
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 
 function Data() {
   const [posts, setPosts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
     // Fetch data from the mock API
@@ -12,11 +13,32 @@ function Data() {
       .then(response => response.json())
       .then(data => setPosts(data.posts));
   }, []);
+  
+  // Identify Unique Categories into an array
+  const uniqueCategories = posts.reduce((uniqueCategories, post) => {
+    post.categories.forEach(eachCategory => { 
+      if (!uniqueCategories.includes(eachCategory.name)) {
+        uniqueCategories.push(eachCategory.name);
+      }
+    });
+    return uniqueCategories;
+  }, []);
+
+
+  // uniqueCategories.map((each) => console.log(each))
 
   const categoriesFormatter = (categories) => {
     const categoryNames = categories.map(category => category.name);
     return <ul>{categoryNames.map(name => <li key={name}>{name}</li>)}</ul>;
   };
+
+  const handleCategorySelect = (event) => {
+    setSelectedCategory(event.target.value || null);
+  };
+
+  const filteredPosts = selectedCategory
+    ? posts.filter(post => post.categories.some(category => category.name === selectedCategory))
+    : posts;
 
   const dateFormatter = (dateString) => {
     const options = {
@@ -37,6 +59,15 @@ function Data() {
       {author.name}
     </div>
   );
+
+  const paginationOptions = {
+    sizePerPageList: [
+      { text: '5', value: 5 },
+      { text: '10', value: 10 },
+      { text: '25', value: 25 },
+      { text: '50', value: 50 }
+    ]
+  };
 
   const columns = [{
     dataField: 'title',
@@ -66,8 +97,26 @@ function Data() {
   return (
     <div style={{ padding: '20px '}}>
       <h2>List Of Posts</h2>
+
+      <Form.Select aria-label="Default select example" onChange={handleCategorySelect}>
+        <option selected>Categories Filter</option>
+        {uniqueCategories.map((category, index) => (
+          <option key={index} value={category}>
+            {category}
+          </option>))
+        }
+      </Form.Select>
+
       <div style={{ fontSize: '10px'}}>
-        <BootstrapTable keyField="id" data={posts} columns={columns} striped hover condensed pagination={paginationFactory()}/>
+        <BootstrapTable 
+          keyField="id" 
+          data={filteredPosts} 
+          columns={columns} 
+          pagination={paginationFactory(paginationOptions)}
+          striped 
+          hover 
+          condensed 
+        />
       </div>
     </div>
   );
